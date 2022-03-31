de6d763590336d5be89fe2f11f2165c3d28056ca
Zhong Yinmin <zhongyinmin@pku.edu.cn>
2022-03-31 15:08:27 +0000

    add running scripts

---
+++ jax/experimental/gda_serialization/run-test.sh
@@ -0,0 +1,1 @@
+python3 test.py --server_addr="172.31.55.51:1456" --num_hosts=2 --host_idx=1
---
+++ jax/experimental/gda_serialization/run1.sh
@@ -0,0 +1,1 @@
+python3 serialization_test.py --server_addr="172.31.55.51:1456" --num_hosts=2 --host_idx=1
